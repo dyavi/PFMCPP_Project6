5ec161cf8a857bf1922e14e1f91a9f299b64d0a0
dyavi <dyayosh@gmail.com>
2025-11-22 23:38:18 +0000

    Initial submission for project 6 part 1

--- main.cpp
+++ main.cpp
@@ -9,48 +9,48 @@ Purpose:  This project will show you the difference between member functions and
  Pointers
 
  Please look at the screenshot in the files to see what happens if you paste this assignment into an existing cpp file in an xcode project
- 
+
  Task: replace all of the <#place holder#> placeholders with proper names and types that will make this program compile and run.
- 
+
  press the Compile, link and run... button and work your way through all of the error messages.
- 
+
 
  steps:
- 
+
  1) finish implementing the constructor for T. the constructor parameters need to initialize 2) and 3)
- 
+
  2) provide a member variable in T of a type of your choosing that works with the compare function below T
          note the name of the variable used in the compare function
- 
+
  3) provide a member variable of type std::string in T that works with the *first* cout statement in main().
          note the name of the variable used in the *first* cout statement in main()
- 
+
  4) finish defining the struct with the compare function
  5) provide proper types for the arguments to compare, and a proper return value
          be sure to make it work with the pointer semantics being used in the if statements.
          see 8) for a hint about what type to use.
- 
+
  6) finish instantiating the two T instances being declared in main(), based on the constructor you finished implementing in T
      Their names (line 105,106) are different than the member variables names (line 77), even tho the placeholders share the same name.
      Don't let this confuse you.
- 
+
  7) Declare 'f' with the proper type so the call to f.compare works
- 
+
  8) call f.compare() with the correct arguments, based on 2) and 5)
- 
+
  9) correct the cout that uses smaller's member variable with some safe pointer usage.
     the possible return values of the compare() function should give you a hint.
     Also: if smaller == nullptr, make the cout statement explain the reasons that `f.compare()` might return nullptr.  
     hint: There are multiple reasons
- 
+
  10) complete the implementation for the static function in <structName2>
- 
+
  11) call the <structName2> static function correctly in main()
- 
+
  12) replicate the functionality of <structName2>'s static function by implementing a member function in U that does the same thing.
- 
+
  13) remember the rules for using pointers!  What is the one thing we always do before we use a pointer?
- 
+
  After you finish, click the [run] button.  Clear up any errors or warnings as best you can.
  */
 
@@ -58,14 +58,16 @@ Purpose:  This project will show you the difference between member functions and
 #include <string>
 struct T
 {
-    T(<#type name#> v, const char* <#variable name#>)   //1
-    //2
-    //3
+    T(int v, const char* n);   //1
+    int value;
+    std::string name;
 };
 
-struct <#structName1#>                                //4
+T::T(int v, const char* n) : value(v), name(n) {}
+
+struct F                                //4
 {
-    <#type name#> compare(<#type name#> a, <#type name#> b) //5
+    T* compare(T* a, T* b) //5
     {
         if( a->value < b->value ) return a;
         if( a->value > b->value ) return b;
@@ -75,37 +77,69 @@ struct <#structName1#>                                //4
 
 struct U
 {
-    float <#name1#> { 0 }, <#name2#> { 0 };
-    <#returnType#> <#memberFunction#>(<#type name#>* <#updatedValue#>)      //12
+    float origin { 0 }, destination { 0 };
+
+    float updateDestinationValue(float* updatedValue)      //12
     {
-        
+        if (updatedValue == nullptr) //13
+        {
+            std::cout << "Warning! The parameter to this function is a null pointer, so the result is defaulting to 0.f now.\n";
+            return 0.f;
+        }
+
+        std::cout << "\n";
+        std::cout << "U's origin value: " << origin << std::endl;
+        origin = *updatedValue;
+        std::cout << "U's origin updated value: " << origin << std::endl;
+        while( std::abs(destination - origin) > 0.001f )
+        {
+            /*
+             write something that makes the distance between that->destination and that->origin get smaller
+             */
+            destination += (origin - destination) * 0.25f;
+        }
+        std::cout << "U's destination updated value: " << destination << std::endl;
+        return destination * origin;        
     }
 };
 
-struct <#structname2#>
+struct V
 {
-    static <#returntype#> <#staticFunctionA#>(U* that, <#type name#>* <#updatedValue#> )        //10
+    static float staticUpdateDestinationValue(U* that, float* updatedValue )        //10
     {
-        std::cout << "U's <#name1#> value: " << that-><#name1#> << std::endl;
-        that-><#name1#> = <#updatedValue#>;
-        std::cout << "U's <#name1#> updated value: " << that-><#name1#> << std::endl;
-        while( std::abs(that-><#name2#> - that-><#name1#>) > 0.001f )
+
+        if (that == nullptr) //13
+        {
+            std::cout << "Warning! The first parameter is a null pointer, so the result is defaulting to 0.f now.\n";
+            return 0.f;
+        }
+        if (updatedValue == nullptr) //13
+        {
+            std::cout << "Warning! The second parameter is a null pointer, so the result is defaulting to 0.f now.\n";
+            return 0.f;
+        }
+
+        std::cout << "\n";
+        std::cout << "U's origin value: " << that->origin << std::endl;
+        that->origin = *updatedValue;
+        std::cout << "U's origin updated value: " << that->origin << std::endl;
+        while( std::abs(that->destination - that->origin) > 0.001f )
         {
             /*
-             write something that makes the distance between that-><#name2#> and that-><#name1#> get smaller
+             write something that makes the distance between that->destination and that->origin get smaller
              */
-            that-><#name2#> += ;
+            that->destination += (that->origin - that->destination) * 0.25f;
         }
-        std::cout << "U's <#name2#> updated value: " << that-><#name2#> << std::endl;
-        return that-><#name2#> * that-><#name1#>;
+        std::cout << "U's destination updated value: " << that->destination << std::endl;
+        return that->destination * that->origin;
     }
 };
-        
+
 /*
  MAKE SURE YOU ARE NOT ON THE MASTER BRANCH
 
  Commit your changes by clicking on the Source Control panel on the left, entering a message, and click [Commit and push].
- 
+
  If you didn't already: 
     Make a pull request after you make your first commit
     pin the pull request link and this repl.it link to our DM thread in a single message.
@@ -117,17 +151,30 @@ struct <#structname2#>
 
 int main()
 {
-    T <#name1#>( , );                                             //6
-    T <#name2#>( , );                                             //6
-    
-    <#structName1#> f;                                            //7
-    auto* smaller = f.compare( , );                              //8
-    std::cout << "the smaller one is << " << smaller->name << std::endl; //9
-    
-    U <#name3#>;
+    T t33(33, "t33");                                             //6
+    T t13(13, "t13");                                             //6
+
+    F f;                                            //7
+    auto* smaller = f.compare(&t33, &t13);                              //8
+
+    if (smaller == nullptr)
+    {
+        std:: cout << "Here are some reasons why `f.compare()` might return nullptr: \n"
+                   << "\tReason 1A: The `value` member variable of both arguments is exactly the same, OR\n"
+                   << "\tReason 1B: passing the same pointer twice as arguments\n"
+                   << "\tReason 2 : One or both arguments are null pointers.\n"
+                   << "\tReason 3 : An instance of T's `value` member variable was not properly initialized and was filled with a garbage.\n"
+                   << "\n";
+    }
+    else 
+    {
+        std::cout << "the smaller one is << " << smaller->name << std::endl; //9
+    }
+
+    U u1;
     float updatedValue = 5.f;
-    std::cout << "[static func] <#name3#>'s multiplied values: " << <#structname2#>::<#staticFunctionA#>( , ) << std::endl;                  //11
-    
-    U <#name4#>;
-    std::cout << "[member func] <#name4#>'s multiplied values: " << <#name4#>.<#memberFunction#>( &updatedValue ) << std::endl;
+    std::cout << "[static func] u1's multiplied values: " << V::staticUpdateDestinationValue(&u1, &updatedValue) << std::endl;                  //11
+
+    U u2;
+    std::cout << "[member func] u2's multiplied values: " << u2.updateDestinationValue(&updatedValue) << std::endl;
 }
